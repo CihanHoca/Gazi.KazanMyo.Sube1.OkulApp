e5346e7093bffe59d1c7d350858e5392bd17b8ab
Cihan Yetişken <cyetisken@gmail.com>
2019-12-16 16:30:26 +0300

    EF Database First

--- Test/Program.cs
+++ Test/Program.cs
@@ -2,6 +2,7 @@
 using Gazi.Sube1.OkulApp.MODELS;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,28 +13,65 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Ogrenci ogr = new Ogrenci();
-            Console.WriteLine("Ogrenci adı giriniz:");
-            ogr.Ad = Console.ReadLine();
-            Console.WriteLine("Soyad giriniz");
-            ogr.Soyad = Console.ReadLine();
-            Console.WriteLine("Numara giriniz");
-            ogr.Numara = int.Parse(Console.ReadLine());
-            ogr.Sinifid = 1;
-            //deneme1
-            OgrenciBL obl = new OgrenciBL();
-            if (obl.Kaydet(ogr))
+            //ORM-Object Relational Mapping
+
+            //OkulContext ctx = new OkulContext();
+            //ctx.tblSiniflars.Add(new tblSiniflar { Kontenjan = 20, SinifAd = "5-A" });
+            //ctx.SaveChanges();
+
+            //using (OkulContext ctx = new OkulContext())
+            //{
+            //    tblSiniflar sinif = ctx.tblSiniflars.Find(4);
+            //    // ctx.tblSiniflars.Remove(sinif);
+            //    sinif.Kontenjan = 45;
+            //    ctx.Entry<tblSiniflar>(sinif).State = EntityState.Modified;
+            //    ctx.SaveChanges();
+            //}
+
+            //using (OkulContext ctx = new OkulContext())
+            //{
+            //    List<tblSiniflar> lst = ctx.tblSiniflars.ToList();
+            //    foreach (tblSiniflar item in lst)
+            //    {
+            //        Console.WriteLine(item.SinifId + " " + item.SinifAd + " " + item.Kontenjan);
+            //    }
+            //}
+
+            //Eager Loading
+            using (OkulContext ctx = new OkulContext())
             {
-                Console.WriteLine("Kayıt Başarılı");
+                List<tblOgrenciler> lst = ctx.tblOgrencilers.Include(o => o.tblSiniflar).ToList();
+
+                foreach (tblOgrenciler item in lst)
+                {
+                    Console.WriteLine(item.Numara + " " + item.Ad + " " + item.Soyad + " " + item.tblSiniflar.SinifAd);
+                }
             }
-            else
-            {
-                Console.WriteLine("Hata");
-            }
+
+            //using (OkulContext ctx=new OkulContext())
+            //{
+            //    List<tblOgrenciler> lst = ctx.tblOgrencilers.Where(ogr => ogr.SinifId == 1).ToList();
+            //    foreach (tblOgrenciler item in lst)
+            //    {
+            //        Console.WriteLine(item.Ad+" "+item.tblSiniflar.SinifAd);
+            //    }
+            //}
+
+            //using (OkulContext ctx=new OkulContext())
+            //{
+            //    foreach (tblSiniflar item in ctx.tblSiniflars.Include(o=>o.tblOgrencilers))
+            //    {
+            //        Console.WriteLine($"Sınıf Ad:{item.SinifAd}");
+
+            //        foreach (tblOgrenciler ogrenci in item.tblOgrencilers)
+            //        {
+            //            Console.WriteLine($"\tÖğrenci Adı:{ogrenci.Ad}");
+            //        }
+            //    }
+            //}
+
 
             Console.ReadKey();
-            
-
         }
     }
 }
